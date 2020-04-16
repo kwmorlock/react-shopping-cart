e700b2a05ebf24a9b1aa62321f161a90353d98ae
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-16 16:21:11 -0400

    cartcontext added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from '.context'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,10 +15,13 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
+
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={cart}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -27,6 +32,8 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+		
+		</ProductContext.Provider>
 		</div>
 	);
 }
